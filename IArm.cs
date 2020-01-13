89621322047f4831478aff21f790b954175e0c76
JonnyHein11 <33763851+JonnyHein11@users.noreply.github.com>
2020-01-12 16:24:06 -0800

    deleted uneccessary files

--- IArm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SightSign
-{
-    public interface IArm
-    {
-        void Connect();
-        void Disconnect();
-        void Move(double x, double y, double z, bool scara);
-    }
-}
