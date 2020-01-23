424ca52327081ef18ddf7f849d26a60c2ff11c8b
zpartain <zpartain@eecs.wsu.edu>
2020-01-22 20:06:37 -0800

    UArmSwift.cs :: integrated into master, but running into issues regarding the Breif.Robotics library. Unable to find documentation of the library.

--- PortDetails.cs
+++ PortDetails.cs
@@ -22,33 +22,6 @@
 
         public static async System.Threading.Tasks.Task<string> FindPortAsync()
         {
-            //var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity");
-
-            //var comPorts = new Dictionary<string, PortDetails>();
-            //foreach (var queryObj in searcher.Get())
-            //{
-            //    if (queryObj["Name"] == null || !queryObj["Name"].ToString().Contains("(COM")) continue;
-
-            //    var portDetails = new PortDetails
-            //    {
-            //        Name = (string)queryObj["Name"],
-            //        PnPId = (string)queryObj["PnPDeviceID"],
-            //        Manufacturer = (string)queryObj["Manufacturer"]
-            //    };
-
-            //    comPorts.Add(portDetails.ComName, portDetails);
-            //}
-
-            //foreach (var port in comPorts.Values)
-            //{
-            //    // uArm using generic windows 10 serial driver
-            //    if (port.PnPId.Contains("FTDIBUS\\VID_0403+PID_6001") || // uArm Metal
-            //        port.PnPId.Contains("USB\\VID_2341&PID_0042"))
-            //    {
-            //        return port.ComName;
-            //    }
-            //}
-
             /* Find all serial devices on the Machine */
             string aqs = SerialDevice.GetDeviceSelector();
             var deviceCollection = await DeviceInformation.FindAllAsync(aqs);
